e8f60baab96dbd5465878f5ab8b60100193c2822
reshete <73260624+reshete@users.noreply.github.com>
2022-04-24 16:03:21 -0400

    final lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -5,12 +5,34 @@ function getRandomIntInclusive(min, max) {
   // The maximum is inclusive and the minimum is inclusive
 }
 
-function dataHandler(dataArray) {
+function restoArrayMake(dataArray) {
   console.log('fired dataHandler');
-  console.table(dataArray);
+ // console.table(dataArray);
   const range = [...Array(15).keys()];
-  range.forEach((item) => {
-    console.log('range item', item);
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1); 
+    return dataArray[restNum];
+  });
+
+// console.log(listItems);
+  return listItems;
+
+  // example forEach for reference
+  // range.forEach((item) => {
+  //  console.log('range item', item);
+  // });
+}
+
+function createHtmlList(collection) {
+  console.log('fired HTML creator');
+  console.log(collection);
+  const targetList = document.querySelector(".resto-list");
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
   });
 }
 async function mainEvent() { // the async keyword means we can make API requests
@@ -21,14 +43,18 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
+
+
   if (arrayFromJson.data.length > 0) {
+    submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
-      submit.style.display = 'block';
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      dataHandler(arrayFromJson.data);
+      const restoArray = restoArrayMake(arrayFromJson.data);
+      createHtmlList(restoArray);
     });
   }
 }
